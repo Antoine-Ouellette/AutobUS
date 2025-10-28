d7aa09b7a2182279ca0f364421ad06789d9285b7
Alexi <leda7624@usherbrooke.ca>
2025-10-28 16:21:00 -0400

    Fonction mur termin peut etre ajuster plus.

--- src/actions_stations.cpp
+++ src/actions_stations.cpp
@@ -4,9 +4,11 @@
  */
 
 #include "actions_stations.h"
-#include <LibRobus.h> // Essentielle pour utiliser RobUS.
+#include <LibRobus.h>           // Essentielle pour utiliser RobUS.
 #include "variables_globales.h" // Inclure les variables globales partagées entre tous les fichiers.
-#include "moteur.h"           // Inclure les fonctions en lien avec les moteurs des roues.
+#include "moteur.h"             // Inclure les fonctions en lien avec les moteurs des roues.
+
+int Etat_mur = 0;
 
 /**
  * Fonction pour seulement avancer jusqu'à retrouver la ligne.
@@ -67,38 +69,136 @@ void renverserQuille()
 void contournerObstacle()
 {
     uint8_t idCapteur = 0;
-    int distanceSeuil = 15; // Distance en cm pour détecter l'obstacle
+    uint16_t distanceSeuil = 15;
     uint16_t distanceActuelle = ROBUS_ReadIR(idCapteur);
-
-    // Le robot avance jusqu'à ce que le capteur ultrason détecte le mur à une distance de ... cm.
-    avancer(0.2);
-    while (distanceSeuil <= distanceActuelle)
+    switch (Etat_mur)
+    {
+    case 0:
+    {
+        setGoal(0.2, AVANCE, 30);
+        Etat_mur = 1;
+        break;
+    }
+    case 1: // Le robot avance jusqu'à ce que le capteur ultrason détecte le mur à une distance de ... cm.
     {
         distanceActuelle = ROBUS_ReadIR(idCapteur);
+        if (distanceSeuil <= distanceActuelle)
+        {
+            arreter();
+            Etat_mur = 2;
+        }
+        break;
     }
-    arreter();
 
-    // Avancer plus loin que l'obstacle
-
-    bool obstacle = true;
-    int distance = 10; // Distance en cm entre chaque essaie
-    while (obstacle)
+    case 2: // Avancer plus loin que l'obstacle
     {
         tourner(RIGHT, 90, 0.2);
-        avancer(0.2); // TODO: avance seulement de 10 cm
-        tourner(LEFT, 90, 0.2);
-        if (ROBUS_ReadIR(idCapteur) > (distanceSeuil + 5))
-            obstacle = false;
+        Etat_mur = 3;
+        break;
     }
 
-    // Contourner l'epaisseur de l'objet avec la distance ...
+    case 3:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 4;
+        }
+        break;
+    }
+    case 4:
+    {
+        setGoal(0.2, AVANCE, 30);
+        Etat_mur = 5;
+        break;
+    }
+    case 5:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 6;
+        }
+        break;
+    }
+    case 6:
+    {
+        tourner(LEFT, 90, 0.2);
+        Etat_mur = 7;
+        break;
+    }
 
-    avancer(0.2);  // TODO: avance seulement de 10 cm
+    case 7:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 8;
+        }
+        break;
+    }
+    case 8:
+    {
+        setGoal(0.2, AVANCE, 30);
+        Etat_mur = 9;
+        break;
+    }
+    case 9:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 10;
+        }
+        break;
+    }
+    case 10:
+    {
+        tourner(LEFT, 90, 0.2);
+        Etat_mur = 11;
+        break;
+    }
 
-    // Revenir jusqu'a la ligne
+    case 11:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 12;
+        }
+        break;
+    }
+    case 12:
+    {
+        setGoal(0.2, AVANCE, 30);
+        Etat_mur = 13;
+        break;
+    }
+    case 13:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 14;
+        }
+        break;
+    }
+    case 14:
+    {
+        tourner(RIGHT, 90, 0.2);
+        Etat_mur = 15;
+        break;
+    }
 
-    tourner(LEFT, 90, 0.2);
-    retrouverLigne();
+    case 15:
+    {
+        if (1 /*isGoal() TODO change*/)
+        {
+            Etat_mur = 16;
+        }
+        break;
+    }
+    case 16:
+    {
+        Etat_mur = 0;
+        currentEtat = SUIVRE_LIGNE;
+        break;
+    }
+    }
 }
 
 /**
